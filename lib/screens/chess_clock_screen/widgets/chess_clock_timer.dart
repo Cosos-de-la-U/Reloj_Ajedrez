03df12b62d79d285b87f5b8019a14bdd633b768c
Raúl Peñate <raul.h.penate@gmail.com>
2021-06-09 02:37:51 -0600

    hola

---
+++ lib/screens/chess_clock_screen/widgets/chess_clock_timer.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+class ChessClockTimer extends StatelessWidget {
+  final bool isReversed;
+  final bool isTicking;
+  final bool isTimeUp;
+  final Function onPressed;
+
+  final Duration availableTime;
+
+  const ChessClockTimer({
+    Key key,
+    this.isReversed = false,
+    this.isTicking = false,
+    this.isTimeUp = false,
+    this.onPressed,
+    @required this.availableTime,
+  }) : super(key: key);
+
+  Color _getColor() {
+    if (isTicking) {
+      return Colors.black;
+    }
+
+    if (isTimeUp) {
+      return Colors.red[900];
+    }
+
+    return Colors.grey[800];
+  }
+
+  String _getAvailabeTimeText() {
+    return availableTime.toString().substring(2, 7);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: onPressed,
+      child: Container(
+        color: _getColor(),
+        child: Center(
+          child: RotatedBox(
+            quarterTurns: isReversed ? 2 : 0,
+            child: Text(
+              _getAvailabeTimeText(),
+              style: TextStyle(
+                fontSize: 70, color: Colors.white,
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
